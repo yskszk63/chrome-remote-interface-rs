9f143fadb541e054ce05878cfdb9d2afb96550d8
yskszk63 <yskszk63@gmail.com>
2021-02-15 22:22:37 +0900

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/chrome-remote-interface/0.1.0-alpha.3")]
+#![doc(html_root_url = "https://docs.rs/chrome-remote-interface/0.1.0-alpha.4")]
 //! [Chrome DevTools Protocol](https://chromedevtools.github.io/devtools-protocol/) client.
 //!
 //! Currently Work In Progress.
