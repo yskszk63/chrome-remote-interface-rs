20f7d39644503e40db203205ee1c5f7d59eaac7b
yskszk63 <yskszk63@gmail.com>
2021-02-20 13:58:28 +0900

    bump version

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/chrome-remote-interface-model/0.1.0-alpha.5")]
+#![doc(html_root_url = "https://docs.rs/chrome-remote-interface-model/0.1.0-alpha.6")]
 //! Chrome DevTools Protocol message model.
 //!
 //! ## License
