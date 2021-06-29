f89153dbf670d9e3e5ed111bc903b72bf2b27468
yskszk63 <yskszk63@gmail.com>
2021-06-29 15:23:34 +0900

    add simple test

---
+++ tests/simple.rs
@@ -0,0 +1,33 @@
+use chrome_remote_interface::Browser;
+use chrome_remote_interface::model::target::{CreateTargetCommand, AttachToTargetCommand};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    let browser = Browser::launcher()
+        .headless(true) // headless mode (Default)
+        .launch()
+        .await?;
+
+    browser.run_with(|mut client| async move {
+        // Open new page
+        let response = client.request(CreateTargetCommand::builder()
+            .url("https://example.org/".into())
+            .build()
+            .unwrap()
+        ).await?;
+
+        // Attach opened page.
+        let response = client
+            .request(AttachToTargetCommand::new((*response).clone(), Some(true)))
+            .await?;
+
+        // construct attached session.
+        let session = client.session(response);
+
+        // DO STUFF
+        // ...
+        drop(session);
+
+        Ok(())
+    }).await
+}
