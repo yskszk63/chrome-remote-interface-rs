e7c24e5cc3f25ca568690351a54b211d3339d2ff
yskszk63 <yskszk63@gmail.com>
2021-06-30 11:15:02 +0900

    typo

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,7 +2,16 @@ use sysinfo::{ProcessExt, RefreshKind, System, SystemExt};
 
 pub fn pgrep_chromium() {
     let sys = System::new_with_specifics(RefreshKind::new().with_processes());
-    for proc in sys.get_process_by_name("vim") {
+    for proc in sys.get_process_by_name("chromium") {
+        println!(
+            "{:?} {} {:?} {}",
+            proc.parent(),
+            proc.pid(),
+            proc.cmd(),
+            proc.status()
+        );
+    }
+    for proc in sys.get_process_by_name("Chromium") {
         println!(
             "{:?} {} {:?} {}",
             proc.parent(),
